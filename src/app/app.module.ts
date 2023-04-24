fd2eb2c8699627d009b3f6af3541ac862598e886
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-04-24 11:05:41 +0300

    Create routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TopicsComponent } from './topics/topics.component';
+import { HomeComponent } from './home/home.component';
+
+
+// определение маршрутов
+const appRoutes: Routes =[
+  { path: '', component: HomeComponent},
+  { path: 'topics', component: TopicsComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopicsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
